5bbb8321021dcfcbe0346185d8440ebf0421d8f2
Apoorv Khanuja <apoorv.khanuja@gmail.com>
2023-04-12 13:01:31 -0400

    Initial commit
    Initial HTML, CSS, and JavaScript files for portfolio content

---
+++ scripts.js
@@ -0,0 +1,27 @@
+// You can add any JavaScript code for interactivity if necessary
+
+const projects = [
+  {
+    title: "Energy Efficiency Measure NLP Analysis",
+    description: "Analyzed Energy Efficiency Measure names using NLP techniques to better understand and categorize them.",
+    imgSrc: "project1.jpg",
+    repoLink: "https://github.com/yourusername/project1"
+  },
+  // Add the remaining projects in a similar format
+];
+
+const projectsSection = document.getElementById("projects");
+
+projects.forEach((project) => {
+  const projectCard = `
+    <div class="project-card">
+      <img src="${project.imgSrc}" alt="${project.title}">
+      <div>
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <a href="${project.repoLink}" target="_blank">View on GitHub</a>
+      </div>
+    </div>
+  `;
+  projectsSection.innerHTML += projectCard;
+});
